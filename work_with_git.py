d267812cb8057733855da96c0c4d62414458090e
Ivan Zerpa <zerpaivan@gmail.com>
2020-12-09 22:43:03 -0400

    avance del proyecto

---
+++ work_with_git.py
@@ -0,0 +1,9 @@
+file = open("names_list.txt", "r")
+text_list = file.readlines()
+file.close()
+
+len_list = len(text_list)
+for i in range(len_list):
+    text_list[i] = text_list[i].rstrip()
+
+print(text_list)
