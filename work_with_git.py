ae17dba450c2d5b53ae7f599ede09d36542494ec
Ivan Zerpa <zerpaivan@gmail.com>
2020-12-11 22:13:22 -0400

    Agregada sentencia if __name__

--- work_with_git.py
+++ work_with_git.py
@@ -1,11 +1,19 @@
 from sort_lists_function import ordenarlista
-file = open("names_list.txt", "r")
+
+try:
+    file = open("namesx_list.txt", "r")
+except:
+    print("Error en archivo")
+    quit()
+
 text_list = file.readlines()
 file.close()
 
 len_list = len(text_list)
+
 for i in range(len_list):
     text_list[i] = text_list[i].rstrip()
 
-print("lista desordenada: ", text_list)
-print("lista ordenada: "ordenarlista(text_list))
+if __name__ == "__main__":
+    print("lista desordenada: ", text_list)
+    print("lista ordenada: "ordenarlista(text_list))
