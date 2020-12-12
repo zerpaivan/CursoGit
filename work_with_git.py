45b4ac0c064a2053548f20cd2db65c3e044de619
Ivan Zerpa <zerpaivan@gmail.com>
2020-12-11 20:52:55 -0400

    se agrego funcion y nombres nuevos

--- work_with_git.py
+++ work_with_git.py
@@ -1,3 +1,4 @@
+from sort_lists_function import ordenarlista
 file = open("names_list.txt", "r")
 text_list = file.readlines()
 file.close()
@@ -7,3 +8,4 @@ for i in range(len_list):
     text_list[i] = text_list[i].rstrip()
 
 print(text_list)
+print(ordenarlista(text_list))
